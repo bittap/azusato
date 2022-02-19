f189640cbb2bdbd55e5a291fecc9e82ce8cd5991
bittap <bittap@naver.com>
2022-02-20 00:44:48 +0900

    test

--- azusato/src/main/java/com/my/azusato/AzusatoApplication.java
+++ azusato/src/main/java/com/my/azusato/AzusatoApplication.java
@@ -1,6 +1,6 @@
 package com.my.azusato;
 
-import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
