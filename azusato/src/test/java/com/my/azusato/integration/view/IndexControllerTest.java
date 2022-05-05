15fb356177121528f48856bcbf680ca16cce1e21
DESKTOP-H0A624F\kim-t <kim-t@tbrave.com>
2022-05-05 11:17:37 +0900

    URL変更によるテストコード修正

--- azusato/src/test/java/com/my/azusato/integration/view/IndexControllerTest.java
+++ azusato/src/test/java/com/my/azusato/integration/view/IndexControllerTest.java
@@ -20,49 +20,40 @@ import com.my.azusato.view.controller.common.ModelConstant;
 import com.my.azusato.view.controller.common.UrlConstant;
 import com.my.azusato.view.controller.response.HeaderReponse;
 
-
 public class IndexControllerTest extends AbstractIntegration {
 
 	@Autowired
 	MockMvc mockMvc;
-	
+
 	private final int MODEL_SIZE = 1 + TestConstant.SPRING_MODEL_SIZE;
-	
+
 	@Nested
-	public class Status_200{
-		
+	public class Status_200 {
+
 		/**
 		 * Check 200 status,model size and model values
 		 */
 		@ParameterizedTest
-		@ValueSource(
-					strings = {UrlConstant.INDEX_CONTROLLER_REQUSET,
-					UrlConstant.INDEX_CONTROLLER_REQUSET+UrlConstant.JAPANESE_CONTROLLER_REQUEST,
-					UrlConstant.INDEX_CONTROLLER_REQUSET+UrlConstant.KOREAN_CONTROLLER_REQUEST}
-				)
+		@ValueSource(strings = { UrlConstant.INDEX_CONTROLLER_REQUSET, UrlConstant.JAPANESE_CONTROLLER_REQUEST,
+				UrlConstant.KOREAN_CONTROLLER_REQUEST })
 		public void givenPath_mapped(String url) throws Exception {
-			MvcResult resultOfMvc =  mockMvc.perform(
-					MockMvcRequestBuilders
-						.get(url)
-					)
-					.andExpect(status().is(200))
+			MvcResult resultOfMvc = mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().is(200))
 					.andReturn();
-			
+
 			ModelAndView mavOfResult = resultOfMvc.getModelAndView();
-			
+
 			Map<String, Object> mapsOfResult = mavOfResult.getModel();
-			mapsOfResult.forEach((k,v)->{
-				System.out.printf("key : %s, value : %s\n",k,v);
+			mapsOfResult.forEach((k, v) -> {
+				System.out.printf("key : %s, value : %s\n", k, v);
 			});
-			
+
 			Assertions.assertEquals(MODEL_SIZE, mapsOfResult.size());
 
-			
-			HeaderReponse headerOfResult = (HeaderReponse)mapsOfResult.get(ModelConstant.HEADER_KEY);
-			
+			HeaderReponse headerOfResult = (HeaderReponse) mapsOfResult.get(ModelConstant.HEADER_KEY);
+
 			HeaderReponse expect = new HeaderReponse();
 			expect.setHome(true);
-			
+
 			Assertions.assertEquals(expect, headerOfResult);
 		}
 	}
