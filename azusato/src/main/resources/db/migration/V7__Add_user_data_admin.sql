1960f79f0914ccd299897997361aebbb1a2ea4da
bittap <bittap@naver.com>
2022-06-05 17:04:24 +0900

    修正

--- azusato/src/main/resources/db/migration/V7__Add_user_data_admin.sql
+++ azusato/src/main/resources/db/migration/V7__Add_user_data_admin.sql
@@ -2,6 +2,6 @@ insert into user(id,name,password,user_type,create_datetime,update_datetime,dele
     ('azuchan7','山本あずさ','{bcrypt}$2a$10$BKeHGsHWkQkwl5I/QwmaFO09wU.nWTk6ivqN42QHdYO/ZdEIszZtO','admin',NOW(),NOW(),'0')
   , ('bittap','김태영','{bcrypt}$2a$10$1U9YQdFgEsjO3JJMHkIS3.z16nnzBUZqpLJEBr.qfegrC2NIW9qhC','admin',NOW(),NOW(),'0');
 
-insert into azusato.profile(image_base64,image_type) values 
+insert into profile(image_base64,image_type) values 
     (null,null)
   , (null,null);
