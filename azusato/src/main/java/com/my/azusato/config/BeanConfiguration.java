644007423deb820585245fac80f54d43c864891f
bittap <bittap@naver.com>
2022-06-05 13:21:29 +0900

    Bean管理一つのクラスでするように修正

--- azusato/src/main/java/com/my/azusato/config/BeanConfiguration.java
+++ azusato/src/main/java/com/my/azusato/config/BeanConfiguration.java
@@ -3,7 +3,9 @@ package com.my.azusato.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
 
+import com.my.azusato.locale.LocalePathAndHeaderResolver;
 import com.my.azusato.property.RestProperty;
 
 import lombok.RequiredArgsConstructor;
@@ -24,5 +26,10 @@ public class BeanConfiguration {
 	public RestTemplate restTemplate() {
 		RestTemplate rt = new RestTemplate();
 		return rt;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new LocalePathAndHeaderResolver();
 	}
 }
