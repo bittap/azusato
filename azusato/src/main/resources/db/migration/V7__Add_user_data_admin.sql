d617356f19e23079e8f113659dcc8efbf7c0abfc
bittap <bittap@naver.com>
2022-06-05 15:31:12 +0900

    admin flyway追加

---
+++ azusato/src/main/resources/db/migration/V7__Add_user_data_admin.sql
@@ -0,0 +1,3 @@
+insert into azusato.`user`(id,name,password,user_type,create_datetime,update_datetime,delete_flag) values 
+    ('azuchan7','山本あずさ','{bcrypt}$2a$10$BKeHGsHWkQkwl5I/QwmaFO09wU.nWTk6ivqN42QHdYO/ZdEIszZtO','admin','2022/06/05 15:24:34','2022/06/05 15:24:37','0')
+  , ('bittap','김태영','{bcrypt}$2a$10$1U9YQdFgEsjO3JJMHkIS3.z16nnzBUZqpLJEBr.qfegrC2NIW9qhC','admin','2022/06/05 15:24:34','2022/06/05 15:24:37','0');
