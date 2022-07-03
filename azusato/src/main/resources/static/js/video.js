6a9f4bce408019ca7b7611095585af975708bd98
bittap <bittap@naver.com>
2022-07-03 14:43:47 +0900

    動画フレームの中にある再生ボタンを押下するとバックエンド音楽の再生を止める。機能追加

--- azusato/src/main/resources/static/js/video.js
+++ azusato/src/main/resources/static/js/video.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	
+	/*
+	 * 動画をクリックすると動画を再生し、バックエンド音楽の再生を止める。
+	 */
 	$('video').on('touchstart click', function(){
 		console.log("video click event");
 		if(this.paused){
@@ -10,6 +13,18 @@ $(document).ready(function(){
 		}
 	});
 	
+	/*
+	 * 動画フレームの中にある再生ボタンを押下するとバックエンド音楽の再生を止める。
+	 */
+	$('video').on('play', function(){
+		console.log("video play event");
+		stopBackgroundMusic();
+	});
+	
+	/*
+	 * バックグラウンド音楽の再生を止める。
+	 * 「#background-audio」がある場合それをstopする。
+	 */
 	function stopBackgroundMusic(){
 		const audio = document.querySelector("#background-audio");
 		// exist a element
