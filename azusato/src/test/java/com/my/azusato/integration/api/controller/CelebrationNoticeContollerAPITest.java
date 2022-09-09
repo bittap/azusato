ec39a5ce1aadd33342d851f46151d924042f3930
bittap <bittap@naver.com>
2022-09-09 19:10:27 +0900

    Junit完了 - CeleNotice

--- azusato/src/test/java/com/my/azusato/integration/api/controller/CelebrationNoticeContollerAPITest.java
+++ azusato/src/test/java/com/my/azusato/integration/api/controller/CelebrationNoticeContollerAPITest.java
@@ -1,120 +1,69 @@
 package com.my.azusato.integration.api.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Locale;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import com.my.azusato.common.TestConstant;
 import com.my.azusato.common.TestLogin;
-import com.my.azusato.exception.ErrorResponse;
 import com.my.azusato.integration.AbstractIntegration;
 
 public class CelebrationNoticeContollerAPITest extends AbstractIntegration {
 
-	final static String RESOUCE_BASIC_PATH = "src/test/data/integration/CelebrationNoticeContollerAPI/";
-	
-	static final String COMMON_URL = "celebration-notice";
-	
-	
-	@Nested
-	class celebrationNotices {
-		
-		final static String RESOUCE_PATH = RESOUCE_BASIC_PATH + "celebrationNotices/";
-		
-		final String URL = COMMON_URL + "s";
-		
-		@Test
-		@DisplayName("正常系_結果200")
-		public void givenNoraml_result200() throws Exception {
-			String folderName = "1";
-			Path initFilePath = Paths.get(RESOUCE_PATH, folderName, TestConstant.INIT_XML_FILE_NAME);
-			dbUnitCompo.initalizeTable(initFilePath);
+  final static String RESOUCE_BASIC_PATH =
+      "src/test/data/integration/CelebrationNoticeContollerAPI/";
 
-			mockMvc.perform(MockMvcRequestBuilders
-					.get(TestConstant.MAKE_ABSOLUTE_URL + TestConstant.API_REQUEST_URL + URL)
-					.with(csrf())
-					.with(user(TestLogin.adminLoginUser())))
-					.andDo(print())
-					.andExpect(status().isOk());
+  static final String COMMON_URL = "celebration-notice";
 
-		}
-		
-		@DisplayName("準正常系_ログインしていない_結果401")
-		@ParameterizedTest
-		@MethodSource("com.my.azusato.common.TestSource#givenNoLogin_result401")
-		public void givenNoLogin_result401(Locale locale, String expectMssage) throws Exception {
-			MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
-					.get(TestConstant.MAKE_ABSOLUTE_URL + TestConstant.API_REQUEST_URL + URL)
-					.with(csrf())
-					.locale(locale))
-					.andDo(print())
-					.andExpect(status().is(401))
-					.andReturn();
 
-			String resultBody = mvcResult.getResponse()
-					.getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
-			ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
+  @Nested
+  class celebrationNotices {
 
-			assertEquals(new ErrorResponse("I-0001", expectMssage),result);
-		}
-	}
+    final static String RESOUCE_PATH = RESOUCE_BASIC_PATH + "celebrationNotices/";
 
-	@Nested
-	class read {
+    final String URL = COMMON_URL + "s";
 
-		final static String RESOUCE_PATH = RESOUCE_BASIC_PATH + "read/";
-		
-		final String CELEBRATION_NO = "1";
-		
-		final String READCOUNTUP_URL = COMMON_URL + "/read/" + CELEBRATION_NO;
+    @Test
+    @DisplayName("正常系_結果200")
+    public void givenNoraml_result200() throws Exception {
+      String folderName = "1";
+      Path initFilePath = Paths.get(RESOUCE_PATH, folderName, TestConstant.INIT_XML_FILE_NAME);
+      dbUnitCompo.initalizeTable(initFilePath);
 
-		@Test
-		@DisplayName("正常系_結果200")
-		public void givenNoraml_result200() throws Exception {
-			String folderName = "1";
-			Path initFilePath = Paths.get(RESOUCE_PATH, folderName, TestConstant.INIT_XML_FILE_NAME);
-			dbUnitCompo.initalizeTable(initFilePath);
+      mockMvc.perform(MockMvcRequestBuilders
+          .get(TestConstant.MAKE_ABSOLUTE_URL + TestConstant.API_REQUEST_URL + URL).with(csrf())
+          .with(user(TestLogin.adminLoginUser()))).andDo(print()).andExpect(status().isOk());
+    }
+  }
 
-			mockMvc.perform(MockMvcRequestBuilders
-					.put(TestConstant.MAKE_ABSOLUTE_URL + TestConstant.API_REQUEST_URL + READCOUNTUP_URL)
-					.with(csrf())
-					.with(user(TestLogin.adminLoginUser()))).andDo(print())
-					.andExpect(status().isOk());
+  @Nested
+  class read {
 
-		}
-		
-		@DisplayName("準正常系_ログインしていない_結果401")
-		@ParameterizedTest
-		@MethodSource("com.my.azusato.common.TestSource#givenNoLogin_result401")
-		public void givenNoLogin_result401(Locale locale, String expectMssage) throws Exception {
-			MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
-					.put(TestConstant.MAKE_ABSOLUTE_URL + TestConstant.API_REQUEST_URL + READCOUNTUP_URL)
-					.with(csrf())
-					.locale(locale))
-					.andDo(print())
-					.andExpect(status().is(401))
-					.andReturn();
+    final static String RESOUCE_PATH = RESOUCE_BASIC_PATH + "read/";
 
-			String resultBody = mvcResult.getResponse()
-					.getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
-			ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
+    final String CELEBRATION_NO = "1";
 
-			assertEquals(new ErrorResponse("I-0001", expectMssage),result);
-		}
-	}
+    final String READCOUNTUP_URL = COMMON_URL + "/read/" + CELEBRATION_NO;
+
+    @Test
+    @DisplayName("正常系_結果200")
+    public void givenNoraml_result200() throws Exception {
+      String folderName = "1";
+      Path initFilePath = Paths.get(RESOUCE_PATH, folderName, TestConstant.INIT_XML_FILE_NAME);
+      dbUnitCompo.initalizeTable(initFilePath);
+
+      mockMvc
+          .perform(MockMvcRequestBuilders
+              .put(TestConstant.MAKE_ABSOLUTE_URL + TestConstant.API_REQUEST_URL + READCOUNTUP_URL)
+              .with(csrf()).with(user(TestLogin.adminLoginUser())))
+          .andDo(print()).andExpect(status().isOk());
+
+    }
+  }
 }
---
+++ azusato/src/test/java/com/my/azusato/unit/api/controller/CelebrationNoticeControllerApiTest.java
@@ -0,0 +1,120 @@
+package com.my.azusato.unit.api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.azusato.api.controller.CelebrationNoticeControllerAPI;
+import com.my.azusato.api.service.CelebrationNoticeServiceAPI;
+import com.my.azusato.common.TestConstant;
+import com.my.azusato.config.WebMvcConfig;
+import com.my.azusato.exception.AzusatoException;
+import com.my.azusato.exception.ErrorResponse;
+import com.my.azusato.view.controller.common.UrlConstant.Api;
+
+@Import(value = InMemoryUserDetailsManager.class)
+@WebMvcTest(controllers = CelebrationNoticeControllerAPI.class,
+    excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+        classes = {WebMvcConfig.class, HandlerInterceptor.class})}) // Interceptor除外
+class CelebrationNoticeControllerApiTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  ObjectMapper om = new ObjectMapper();
+
+  @MockBean
+  CelebrationNoticeServiceAPI mockCeleNoticeService;
+
+  @Nested
+  class celebrationNotices {
+
+    @Nested
+    @DisplayName("正常系")
+    @Disabled("結合テストで対応")
+    class normal {
+
+    }
+
+    @Nested
+    @DisplayName("準正常系")
+    class subnormal {
+
+      @ParameterizedTest
+      @MethodSource("com.my.azusato.common.TestSource#I0001_Message")
+      @DisplayName("セッションがない_結果401をスローする")
+      public void givenNoSession_resultThrow401(Locale locale, String expectMessage)
+          throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+            .get(TestConstant.MAKE_ABSOLUTE_URL + Api.COMMON_REQUSET
+                + CelebrationNoticeControllerAPI.CELEBRATION_NOTICES_URL)
+            .with(csrf()).locale(locale)).andDo(print()).andExpect(status().is(401)).andReturn();
+
+        String resultBody = mvcResult.getResponse()
+            .getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
+        ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
+
+        assertEquals(new ErrorResponse(AzusatoException.I0001, expectMessage), result);
+      }
+    }
+  }
+
+  @Nested
+  class read {
+
+    final String CELEBRATION_REPLY_NO = "1";
+
+    final String CELEBRATION_NO = "1";
+
+    final String READCOUNTUP_URL =
+        CelebrationNoticeControllerAPI.COMMON_URL + "/read/" + CELEBRATION_NO;
+
+    @Nested
+    @DisplayName("正常系")
+    @Disabled("結合テストで対応")
+    class normal {
+
+    }
+
+    @Nested
+    @DisplayName("準正常系")
+    class subnormal {
+
+      @ParameterizedTest
+      @MethodSource("com.my.azusato.common.TestSource#I0001_Message")
+      @DisplayName("セッションがない_結果401をスローする")
+      public void givenNoSession_result401(Locale locale, String expectMessage) throws Exception {
+        MvcResult mvcResult = mockMvc
+            .perform(MockMvcRequestBuilders
+                .put(TestConstant.MAKE_ABSOLUTE_URL + Api.COMMON_REQUSET + READCOUNTUP_URL)
+                .with(csrf()).locale(locale))
+            .andDo(print()).andExpect(status().is(401)).andReturn();
+
+        String resultBody = mvcResult.getResponse()
+            .getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
+        ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
+
+        assertEquals(new ErrorResponse(AzusatoException.I0001, expectMessage), result);
+      }
+    }
+  }
+}
