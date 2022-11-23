8a2c53d030fc671be18b30b9e4790461ba7d4469
bittap <kinjwo1@gmail.com>
2022-11-23 17:55:56 +0900

    微修正。行改善

--- azusato/src/main/java/com/my/azusato/interceptor/NavigationInterceptor.java
+++ azusato/src/main/java/com/my/azusato/interceptor/NavigationInterceptor.java
@@ -1,21 +1,17 @@
 package com.my.azusato.interceptor;
 
 import java.util.Objects;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.my.azusato.api.controller.request.MyPageControllerRequest;
 import com.my.azusato.api.service.CelebrationNoticeServiceAPI;
 import com.my.azusato.api.service.request.GetCelebrationsSerivceAPIRequset;
 import com.my.azusato.api.service.response.GetCelebrationNoticesSerivceAPIResponse;
 import com.my.azusato.login.LoginUser;
 import com.my.azusato.util.LoginUtil;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,36 +25,35 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class NavigationInterceptor implements HandlerInterceptor {
-	
-	public static final String MODEL_MAP_NAME = "nav";
-	
-	private final CelebrationNoticeServiceAPI celeNotiAPIService;
 
-	/**
-	 * お祝い通知データを{@link NavigationInterceptor#MODEL_MAP_NAME}に詰める。
-	 * ログインしていないと何もしない。
-	 */
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		if(Objects.nonNull(modelAndView)) {
-			if(Objects.nonNull(request.getUserPrincipal())) {
-				MyPageControllerRequest page = new MyPageControllerRequest(1, 3, 5);
-				
-				GetCelebrationsSerivceAPIRequset serviceReq = GetCelebrationsSerivceAPIRequset.builder()
-										.pageReq(page)
-										.build();
-				LoginUser loginUser = LoginUtil.getLoginUser();
-				GetCelebrationNoticesSerivceAPIResponse result = celeNotiAPIService.celebrationNotices(serviceReq,loginUser.getUSER_NO());
-				modelAndView.addObject(MODEL_MAP_NAME, result);
-				log.debug("お祝い通知(nav):対象:{}",result);
-			}else {
-				log.debug("お祝い通知(nav):ログインしていないため非対象");
-			}
-			
-		}else {
-			log.debug("お祝い通知(nav):modelAndViewがないため非対象");
-		}
-		
-	}
+  public static final String MODEL_MAP_NAME = "nav";
+
+  private final CelebrationNoticeServiceAPI celeNotiAPIService;
+
+  /**
+   * お祝い通知データを{@link NavigationInterceptor#MODEL_MAP_NAME}に詰める。 ログインしていないと何もしない。
+   */
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+      ModelAndView modelAndView) throws Exception {
+    if (Objects.nonNull(modelAndView)) {
+      if (Objects.nonNull(request.getUserPrincipal())) {
+        MyPageControllerRequest page = new MyPageControllerRequest(1, 3, 5);
+
+        GetCelebrationsSerivceAPIRequset serviceReq =
+            GetCelebrationsSerivceAPIRequset.builder().pageReq(page).build();
+        LoginUser loginUser = LoginUtil.getLoginUser();
+        GetCelebrationNoticesSerivceAPIResponse result =
+            celeNotiAPIService.celebrationNotices(serviceReq, loginUser.getUSER_NO());
+        modelAndView.addObject(MODEL_MAP_NAME, result);
+        log.debug("お祝い通知(nav):対象:{}", result);
+      } else {
+        log.debug("お祝い通知(nav):ログインしていないため非対象");
+      }
+
+    } else {
+      log.debug("お祝い通知(nav):modelAndViewがないため非対象");
+    }
+
+  }
 }
