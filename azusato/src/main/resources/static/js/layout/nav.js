4e38451d4a78212647c9ab0ca541f7a5bc77a835
bittap <kinjwo1@gmail.com>
2023-01-05 13:11:28 +0900

    クリックツールチップ削除

--- azusato/src/main/resources/static/js/layout/nav.js
+++ azusato/src/main/resources/static/js/layout/nav.js
@@ -2,17 +2,6 @@
 *	/layout/nav.html関連JS
 */
 
-// お祝いメニューは常にツールチップを表示するように
-const NAV_CELEBRATION_TOOLTIP = new bootstrap.Tooltip(document.querySelector('#nav_celebration'), {
-	trigger:"manual", // プログラムでツールチップが表示されるように
-	offset:"0,3",
-	container: 'body',
-	placement: 'bottom',
-});
-console.log(NAV_CELEBRATION_TOOLTIP);
-NAV_CELEBRATION_TOOLTIP.show();
-
-
 /*
 * お祝い通知リストを描く。
 * 既存に表示されているリストは削除し再読み込み。
