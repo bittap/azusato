803a3250eb8d30111462d62142cdaefef363bb72
bittap <kinjwo1@naver.com>
2023-01-12 23:26:43 +0900

    Service単体テストで行った二重テスト削除

--- azusato/src/test/java/com/my/azusato/integration/api/controller/CelebrationReplyContollerAPITest.java
+++ azusato/src/test/java/com/my/azusato/integration/api/controller/CelebrationReplyContollerAPITest.java
@@ -1,30 +1,19 @@
 package com.my.azusato.integration.api.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Locale;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.my.azusato.api.controller.CelebrationReplyControllerAPI;
 import com.my.azusato.api.controller.request.AddCelebrationReplyAPIReqeust;
 import com.my.azusato.common.TestConstant;
 import com.my.azusato.common.TestConstant.Entity;
 import com.my.azusato.common.TestLogin;
-import com.my.azusato.entity.CelebrationEntity;
-import com.my.azusato.entity.CelebrationReplyEntity;
-import com.my.azusato.entity.UserEntity;
-import com.my.azusato.exception.AzusatoException;
-import com.my.azusato.exception.ErrorResponse;
 import com.my.azusato.integration.AbstractIntegration;
 import com.my.azusato.view.controller.common.HttpConstant;
 import com.my.azusato.view.controller.common.UrlConstant.Api;
@@ -73,56 +62,6 @@ public class CelebrationReplyContollerAPITest extends AbstractIntegration {
       }
     }
 
-    @ParameterizedTest
-    @MethodSource("com.my.azusato.common.TestSource#locales")
-    public void givenNoCelebrationdata_result400(Locale locale) throws Exception {
-      Path initFilePath =
-          Paths.get(AddReplyCelebration.RESOUCE_PATH, "2", TestConstant.INIT_XML_FILE_NAME);
-      dbUnitCompo.initalizeTable(initFilePath);
-
-      MvcResult mvcResult = mockMvc
-          .perform(MockMvcRequestBuilders
-              .post(TestConstant.MAKE_ABSOLUTE_URL + Api.COMMON_REQUSET
-                  + CelebrationReplyControllerAPI.COMMON_URL + "/" + "1000")
-              .with(csrf()).content(getRequestBody())
-              .contentType(HttpConstant.DEFAULT_CONTENT_TYPE_STRING)
-              .with(user(TestLogin.adminLoginUser())).locale(locale))
-          .andDo(print()).andExpect(status().is(400)).andReturn();
-
-      String resultBody =
-          mvcResult.getResponse().getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
-      ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
-      String tableName = messageSource.getMessage(CelebrationEntity.TABLE_NAME_KEY, null, locale);
-
-      assertEquals(
-          new ErrorResponse(AzusatoException.I0005,
-              messageSource.getMessage(AzusatoException.I0005, new String[] {tableName}, locale)),
-          result);
-    }
-
-    @ParameterizedTest
-    @MethodSource("com.my.azusato.common.TestSource#locales")
-    public void givenNoUserdata_result400(Locale locale) throws Exception {
-      MvcResult mvcResult = mockMvc
-          .perform(MockMvcRequestBuilders
-              .post(TestConstant.MAKE_ABSOLUTE_URL + Api.COMMON_REQUSET
-                  + CelebrationReplyControllerAPI.COMMON_URL + "/" + "1000")
-              .with(csrf()).content(getRequestBody())
-              .contentType(HttpConstant.DEFAULT_CONTENT_TYPE_STRING)
-              .with(user(TestLogin.adminLoginUser())).locale(locale))
-          .andDo(print()).andExpect(status().is(400)).andReturn();
-
-      String resultBody =
-          mvcResult.getResponse().getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
-      ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
-      String tableName = messageSource.getMessage(UserEntity.TABLE_NAME_KEY, null, locale);
-
-      assertEquals(
-          new ErrorResponse(AzusatoException.I0005,
-              messageSource.getMessage(AzusatoException.I0005, new String[] {tableName}, locale)),
-          result);
-    }
-
     private String getRequestBody() throws Exception {
       AddCelebrationReplyAPIReqeust req = AddCelebrationReplyAPIReqeust.builder()
           .name(Entity.updatedVarChars[0]).content(Entity.createdVarChars[0]).build();
@@ -167,50 +106,6 @@ public class CelebrationReplyContollerAPITest extends AbstractIntegration {
               TestConstant.DEFAULT_EXCLUDE_UPDATE_DATE_COLUMNS);
         }
       }
-    }
-
-    @ParameterizedTest
-    @MethodSource("com.my.azusato.common.TestSource#locales")
-    public void givenDifferenceUser_result400(Locale locale) throws Exception {
-      Path initFilePath =
-          Paths.get(DeleteCeleReplybration.RESOUCE_PATH, "2", TestConstant.INIT_XML_FILE_NAME);
-      dbUnitCompo.initalizeTable(initFilePath);
-
-      MvcResult mvcResult = mockMvc
-          .perform(MockMvcRequestBuilders
-              .delete(TestConstant.MAKE_ABSOLUTE_URL + Api.COMMON_REQUSET
-                  + CelebrationReplyControllerAPI.COMMON_URL + "/" + CELEBRATION_REPLY_NO)
-              .with(csrf()).locale(locale).with(user(TestLogin.adminLoginUser())))
-          .andDo(print()).andExpect(status().isBadRequest()).andReturn();
-
-      String resultBody =
-          mvcResult.getResponse().getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
-      ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
-
-      assertEquals(new ErrorResponse(AzusatoException.I0006,
-          messageSource.getMessage(AzusatoException.I0006, null, locale)), result);
-    }
-
-    @ParameterizedTest
-    @MethodSource("com.my.azusato.common.TestSource#locales")
-    public void givenNodata_result400(Locale locale) throws Exception {
-      MvcResult mvcResult = mockMvc
-          .perform(MockMvcRequestBuilders
-              .delete(TestConstant.MAKE_ABSOLUTE_URL + Api.COMMON_REQUSET
-                  + CelebrationReplyControllerAPI.COMMON_URL + "/" + "1000")
-              .with(csrf()).locale(locale).with(user(TestLogin.adminLoginUser())))
-          .andDo(print()).andExpect(status().isBadRequest()).andReturn();
-
-      String resultBody =
-          mvcResult.getResponse().getContentAsString(Charset.forName(TestConstant.DEFAULT_CHARSET));
-      ErrorResponse result = om.readValue(resultBody, ErrorResponse.class);
-      String tableName =
-          messageSource.getMessage(CelebrationReplyEntity.TABLE_NAME_KEY, null, locale);
-
-      assertEquals(
-          new ErrorResponse(AzusatoException.I0005,
-              messageSource.getMessage(AzusatoException.I0005, new String[] {tableName}, locale)),
-          result);
     }
   }
 
