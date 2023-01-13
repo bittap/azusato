bcaa459d127380e32816024159bc0ebb549516b6
bittap <kinjwo1@naver.com>
2023-01-14 00:40:56 +0900

    DBUNIT削除

--- azusato/src/test/java/com/my/azusato/integration/interceptor/NavigationInterceptorTest.java
+++ azusato/src/test/java/com/my/azusato/integration/interceptor/NavigationInterceptorTest.java
@@ -2,11 +2,7 @@ package com.my.azusato.integration.interceptor;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -14,72 +10,61 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.my.azusato.api.service.response.GetCelebrationNoticesSerivceAPIResponse;
 import com.my.azusato.common.TestConstant;
-import com.my.azusato.common.TestLogin;
 import com.my.azusato.integration.AbstractIntegration;
+import com.my.azusato.interceptor.NavigationInterceptor;
+import com.my.azusato.login.LoginUser;
 
 public class NavigationInterceptorTest extends AbstractIntegration {
-	
-	private final String NO_EXIST_URL = "test";
-	
-	final static String RESOUCE_BASIC_PATH = "src/test/data/integration/interceptor/NavigationInterceptor/";
 
-	@Nested
-	class postHandle{
-		
-		final static String RESOUCE_PATH = RESOUCE_BASIC_PATH + "postHandle/";
-		
-		final String NAV_MODEL_KEY = "nav";
-		
-		@Test
-		@DisplayName("正常系_404ページでモデルがない_結果modelはnull")
-		void givenNoModel_resultModelIsNull() throws Exception{
-			MvcResult resultOfMvc = mockMvc.perform(MockMvcRequestBuilders.get(TestConstant.MAKE_ABSOLUTE_URL + NO_EXIST_URL))
-			.andDo(print())
-			.andReturn();
-			
-			ModelAndView mavOfResult = resultOfMvc.getModelAndView();
+  @Nested
+  class postHandle {
 
-			Assertions.assertNull(mavOfResult);
-		}
-	
-		@Test
-		@DisplayName("正常系_ログインしていない_結果キーnavModelはnull")
-		void givenNoLogin_resultModelNavIsNull() throws Exception{
-			MvcResult resultOfMvc = mockMvc.perform(MockMvcRequestBuilders.get(TestConstant.MAKE_ABSOLUTE_URL))
-			.andDo(print())
-			.andReturn();
-			
-			ModelAndView mavOfResult = resultOfMvc.getModelAndView();
+    @Nested
+    @DisplayName("正常系")
+    class normal {
+      @Test
+      @DisplayName("404ページでモデルがない_結果modelはnull")
+      void givenNoModel_whenInvoke404Page_resultModelIsNull() throws Exception {
+        MvcResult resultOfMvc =
+            mockMvc.perform(MockMvcRequestBuilders.get(TestConstant.MAKE_ABSOLUTE_URL))
+                .andDo(print()).andReturn();
 
-			Map<String, Object> mapsOfResult = mavOfResult.getModel();
-			
-			Assertions.assertNull(mapsOfResult.get(NAV_MODEL_KEY));
-		}
-		
-		@Test
-		@DisplayName("正常系_ログインしていてお祝い通知2件ある_結果キーnavModelの結果2件")
-		void givenLoginAndCelebrationNotice2data_resultModelNavIs2data() throws Exception{
-			String folderName = "1";
-			Path initFilePath = Paths.get(RESOUCE_PATH, folderName, TestConstant.INIT_XML_FILE_NAME);
-			dbUnitCompo.initalizeTable(initFilePath);
-			
-			MvcResult resultOfMvc = mockMvc.perform(
-					MockMvcRequestBuilders.get(TestConstant.MAKE_ABSOLUTE_URL)
-					.with(user(TestLogin.adminLoginUser())))
-			.andDo(print())
-			.andReturn();
-			
-			ModelAndView mavOfResult = resultOfMvc.getModelAndView();
+        assertNavModelIsNull(resultOfMvc.getModelAndView());
+      }
 
-			Map<String, Object> mapsOfResult = mavOfResult.getModel();
-			
-			Assertions.assertNotNull(mapsOfResult.get(NAV_MODEL_KEY));
-			
-			GetCelebrationNoticesSerivceAPIResponse result = (GetCelebrationNoticesSerivceAPIResponse)mapsOfResult.get(NAV_MODEL_KEY);
-			Assertions.assertEquals(2,result.getNotices().size());
-		}
-	}
+      @Test
+      @DisplayName("ログインしていない_結果キーnavModelはnull")
+      void givenNoLogin_resultModelNavIsNull() throws Exception {
+        MvcResult resultOfMvc =
+            mockMvc.perform(MockMvcRequestBuilders.get(TestConstant.MAKE_ABSOLUTE_URL))
+                .andDo(print()).andReturn();
+
+        assertNavModelIsNull(resultOfMvc.getModelAndView());
+      }
+
+      @Test
+      @DisplayName("ログインしていてお祝い通知2件ある_結果キーnavModelの結果2件")
+      void givenLoginAndCelebrationNotice2data_resultModelNavIs2data() throws Exception {
+        MvcResult resultOfMvc =
+            mockMvc.perform(MockMvcRequestBuilders.get(TestConstant.MAKE_ABSOLUTE_URL)
+                .with(user(new LoginUser(getAdminUser())))).andDo(print()).andReturn();
+
+        ModelAndView mavOfResult = resultOfMvc.getModelAndView();
+        Map<String, Object> mapsOfResult = mavOfResult.getModel();
+        Assertions.assertNotNull(mapsOfResult.get(NavigationInterceptor.MODEL_MAP_NAME));
+
+        GetCelebrationNoticesSerivceAPIResponse result =
+            (GetCelebrationNoticesSerivceAPIResponse) mapsOfResult
+                .get(NavigationInterceptor.MODEL_MAP_NAME);
+        Assertions.assertEquals(2, result.getNotices().size());
+      }
+
+      private void assertNavModelIsNull(ModelAndView mavOfResult) {
+        Map<String, Object> mapsOfResult = mavOfResult.getModel();
+        Assertions.assertNull(mapsOfResult.get(NavigationInterceptor.MODEL_MAP_NAME));
+      }
+    }
+  }
 }
