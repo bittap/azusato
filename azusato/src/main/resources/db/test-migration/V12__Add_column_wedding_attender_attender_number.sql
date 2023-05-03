1807ba633318bd8c2c853f46d927c26e30ec63de
bittap <kinjwo1@naver.com>
2023-05-04 00:11:39 +0900

    コメントでNOT NULL DEFAULTがないため、その制約がなくなる不具合対応

--- azusato/src/main/resources/db/test-migration/V12__Add_column_wedding_attender_attender_number.sql
+++ azusato/src/main/resources/db/test-migration/V12__Add_column_wedding_attender_attender_number.sql
@@ -1,3 +1,2 @@
 -- 結婚式参加テーブルにカラムを追加
-ALTER TABLE wedding_attender ADD COLUMN attender_number TINYINT default 1 NOT NULL;
-ALTER TABLE wedding_attender MODIFY attender_number TINYINT COMMENT '参加者数';
+ALTER TABLE wedding_attender ADD COLUMN attender_number TINYINT default '1' NOT NULL COMMENT '参加者数';
