feeb8d5b7dbff6f3fb6962b639831a7dd4fb3e31
bittap <kinjwo1@naver.com>
2023-11-28 08:45:11 +0900

    bugspotsの「equals メソッドをオーバーライドしていますが，対称的ではないかもしれません。」対応
    このクラスはスーパークラスの equals メソッドをオーバーライドする equals メソッドを定義しています。 両方の equals メソッドは，2つのオブジェクトが等しいかどうかの判定で， instanceof を使用しています。 equals メソッドは対称的 (a.equals(b) == b.equals(a)) であることが重要なのでこれは危険を伴っています。 B が A のサブタイプなら A の equals メソッドは引数が instanceof A なのかチェックします。 そして， B の equals メソッドは引数が instanceof B なのかチェックします。 これらのメソッドによって定義された同値関係が対称的ではないということです。

--- azusato/src/main/java/com/my/azusato/login/LoginUser.java
+++ azusato/src/main/java/com/my/azusato/login/LoginUser.java
@@ -2,36 +2,34 @@ package com.my.azusato.login;
 
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.security.core.userdetails.User;
-
 import com.my.azusato.entity.UserEntity;
 import com.my.azusato.entity.UserEntity.Type;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = true)
 public class LoginUser extends User {
-	
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * accountNonExpired,credentialsNonExpired等使用しないのは"true"
-	 */
-	private static final boolean NOT_USED_FIELD = true;
-	
-	private final Long USER_NO;
-	
-	private static final String NOT_USE_PASSWORD = "NonMemberはパスワードを使用しない。";
+  private static final long serialVersionUID = 1L;
 
-	public LoginUser(UserEntity user) {
-		super(user.getId(), Strings.isEmpty(user.getPassword()) ? NOT_USE_PASSWORD : user.getPassword(), !user.getCommonFlag().getDeleteFlag(), 
-				NOT_USED_FIELD, NOT_USED_FIELD, NOT_USED_FIELD, Grant.resolveRoles(Type.valueOf(user.getUserType())));
-		
-		this.USER_NO = user.getNo();
-	}
-	
-	
-	
+  /**
+   * accountNonExpired,credentialsNonExpired等使用しないのは"true"
+   */
+  private static final boolean NOT_USED_FIELD = true;
+
+  private final Long USER_NO;
+
+  private static final String NOT_USE_PASSWORD = "NonMemberはパスワードを使用しない。";
+
+  public LoginUser(UserEntity user) {
+    super(user.getId(), Strings.isEmpty(user.getPassword()) ? NOT_USE_PASSWORD : user.getPassword(),
+        !user.getCommonFlag().getDeleteFlag(), NOT_USED_FIELD, NOT_USED_FIELD, NOT_USED_FIELD,
+        Grant.resolveRoles(Type.valueOf(user.getUserType())));
+
+    this.USER_NO = user.getNo();
+  }
+
+
 
 }
