0f641f11eed06d791aa31f94b98eb51794a0b701
bittap <kinjwo1@naver.com>
2024-01-07 14:43:43 +0900

    swagger-uiを追加

--- azusato/src/main/java/com/my/azusato/AzusatoApplication.java
+++ azusato/src/main/java/com/my/azusato/AzusatoApplication.java
@@ -3,7 +3,6 @@ package com.my.azusato;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
 import com.my.azusato.property.CelebrationNoticeProperty;
 import com.my.azusato.property.CelebrationProperty;
 import com.my.azusato.property.CookieProperty;
@@ -11,16 +10,20 @@ import com.my.azusato.property.ProfileProperty;
 import com.my.azusato.property.RestProperty;
 import com.my.azusato.property.SessionProperty;
 import com.my.azusato.property.UserProperty;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 // for using @ConfigurationProperties
-@EnableConfigurationProperties(value = { UserProperty.class, ProfileProperty.class, 
-		SessionProperty.class, RestProperty.class , CookieProperty.class,
-		CelebrationProperty.class , CelebrationNoticeProperty.class})
+@EnableConfigurationProperties(
+    value = {UserProperty.class, ProfileProperty.class, SessionProperty.class, RestProperty.class,
+        CookieProperty.class, CelebrationProperty.class, CelebrationNoticeProperty.class})
+@OpenAPIDefinition(info = @Info(title = "あずさとAPI", //
+    version = "v1")) //
 public class AzusatoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AzusatoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AzusatoApplication.class, args);
+  }
 
 }
--- azusato/src/main/java/com/my/azusato/api/controller/WeddingAttenderControllerAPI.java
+++ azusato/src/main/java/com/my/azusato/api/controller/WeddingAttenderControllerAPI.java
@@ -14,27 +14,46 @@ import com.my.azusato.api.controller.request.CreateWeddingAttendRequest;
 import com.my.azusato.api.controller.request.GetWeddingAttendsRequest;
 import com.my.azusato.api.service.WeddingAttenderServiceAPI;
 import com.my.azusato.api.service.response.GetWeddingAttenderServiceAPIResponse;
+import com.my.azusato.exception.ErrorResponse;
 import com.my.azusato.view.controller.common.UrlConstant.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(value = Api.COMMON_REQUSET + "wedding/")
 @RequiredArgsConstructor
+@Tag(name = "結婚式参加者")
 public class WeddingAttenderControllerAPI {
 
   private final WeddingAttenderServiceAPI weddingAttendService;
 
+  @Operation(summary = "API_wed_attend_001 結婚式参加者の登録", description = "結婚式参加者の登録を行う")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "成功"),
+      @ApiResponse(responseCode = "400", description = "パラメータがエラー", //
+          content = {@Content(mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class))})})
   @PostMapping("attender")
   @ResponseStatus(HttpStatus.CREATED)
-  @MethodAnnotation(description = "API_wed_attend_001 結婚式参加の登録")
-  public void create(@RequestBody @Validated CreateWeddingAttendRequest request) {
+  @MethodAnnotation(description = "API_wed_attend_001 結婚式参加者の登録")
+  public void create( //
+      @Parameter(description = "結婚式参加者情報", required = true) //
+      @RequestBody @Validated CreateWeddingAttendRequest request) {
     weddingAttendService.create(request);
   }
 
+  @Operation(summary = "API_wed_attend_002 結婚式参加者リスト", description = "結婚式参加者リストを取得する")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "結婚式参加者リスト"),})
   @GetMapping("attenders")
   @ResponseStatus(HttpStatus.OK)
   @MethodAnnotation(description = "API_wed_attend_002 結婚式参加者リスト")
   public GetWeddingAttenderServiceAPIResponse get(
+      @Parameter(description = "検索条件とページ情報", required = true) //
       @ModelAttribute @Validated GetWeddingAttendsRequest request) {
     return weddingAttendService.get(request);
   }
